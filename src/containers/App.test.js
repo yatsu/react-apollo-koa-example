d817f400e14ae9a73bb98e7eb9392d20bb5a06ca
Masaki Yatsu <yatsu@yatsu.info>
2017-03-04 13:55:56 +0900

    Test with Enzyme

--- src/containers/App.test.js
+++ src/containers/App.test.js
@@ -1,8 +1,19 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
 import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App/>, div)
+function setup() {
+  const app = shallow(<App/>)
+
+  return {
+    app
+  }
+}
+
+describe('<App/>', () => {
+  const { app } = setup()
+
+  it('renders div', () => {
+    expect(app.find('div').hasClass('app')).toBe(true)
+  })
 })
