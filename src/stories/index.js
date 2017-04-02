f609db8b6e0880ac21c248efd711a8a218f088a0
Masaki Yatsu <yatsu@yatsu.info>
2017-04-03 00:41:19 +0900

    Add Storyshots

--- src/stories/index.js
+++ src/stories/index.js
@@ -4,17 +4,21 @@ import Header from '../components/Header'
 import Signin from '../components/Signin/Signin'
 
 storiesOf('Header', module)
-  .add('without auth', () => <Header path={'/'} signout={action('signout')} />)
-  .add('with normal user', () => <Header path={'/'} username={'bob'} signout={action('signout')} />)
+  .add('without auth', () => <Header path={'/'} admin={false} signout={action('signout')} />)
+  .add('with normal user', () => (
+    <Header path={'/'} username={'bob'} admin={false} signout={action('signout')} />
+  ))
   .add('with admin user', () => (
     <Header path={'/'} username={'bob'} admin signout={action('signout')} />
   ))
-  .add('Local tab', () => <Header path={'/todo'} username={'bob'} signout={action('signout')} />)
+  .add('Local tab', () => (
+    <Header path={'/todo'} username={'bob'} admin={false} signout={action('signout')} />
+  ))
   .add('GraphQL tab', () => (
-    <Header path={'/todo-remote'} username={'bob'} signout={action('signout')} />
+    <Header path={'/todo-remote'} username={'bob'} admin={false} signout={action('signout')} />
   ))
   .add('GraphQL Subscription tab', () => (
-    <Header path={'/todo-pubsub'} username={'bob'} signout={action('signout')} />
+    <Header path={'/todo-pubsub'} username={'bob'} admin={false} signout={action('signout')} />
   ))
 
 storiesOf('Signin', module)
