9d54b7d74791f4432168dc64d5209c1bd063e624
Masaki Yatsu <yatsu@yatsu.info>
2017-06-10 19:47:26 +0900

    Fix an error on calling apollo-client

--- src/apollo/create-apollo-client.js
+++ src/apollo/create-apollo-client.js
@@ -1,12 +1,12 @@
 // @flow
-import ApolloClient, { addTypename } from 'apollo-client'
+import ApolloClient from 'apollo-client'
 
 export default (options: Object) =>
   new ApolloClient(
     Object.assign(
       {},
       {
-        queryTransformer: addTypename,
+        addTypename: true,
         dataIdFromObject: (result) => {
           if (result.id && result.__typename) {
             return result.__typename + result.id
